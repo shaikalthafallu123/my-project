9dd223d0ad68e89dd321118e1a96394a4c26e4b8
shaikalthafallu123 <77638431+shaikalthafallu123@users.noreply.github.com>
2021-01-19 10:08:06 +0530

    Update external.js

--- external.js
+++ external.js
@@ -1,1 +1,14 @@
-documnt.write("welcome to java script file");
+<!DOCTYPE html>
+  <html>
+  <head>file
+  </head>
+<body>
+  <script>
+  var emp=new array("althaf","allu")
+for(i=0;i<emp.length;i++){
+
+documnt.write("emp[i]+<br/>");
+}
+</script>
+</body>
+<html>
